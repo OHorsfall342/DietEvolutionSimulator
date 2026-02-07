186b154181d5612071c76b91ee9c8e15238881be
OHorsfall342 <oscar.horsfall342@gmail.com>
2026-02-07 16:10:17 +0000

    Updated global variables to standard naming scheme

--- Darwin/Globals.cs
+++ Darwin/Globals.cs
@@ -2,16 +2,18 @@ namespace Darwin
 {
     public static class Globals
     {
-        public const int totaldays = 15000;
-        public const int actionsperday = 10; // amount of actions in a day, standard speed of 10 would compelte 1, speed of 5 would complete 2 etc
-        public const int mapsize = 12;
-        public const int plantval = 60;
-        public const int meatval = 200;
-        public const int mutationchance = 10;
-        public const float mutationAmount = 0.1f;
-        public const int maxSpeed = 10;
-        public const float herbivoreThreshold = 0.3f;
-        public const float carnivoreThreshold = 0.7f;
-        public const int foodchance = 7;//change these values to see different effects
+        public const int TotalDays = 15000;
+        public const int ActionsPerDay = 10; // amount of actions in a day, standard speed of 10 would compelte 1, speed of 5 would complete 2 etc
+        public const int MapSize = 12;
+        public const int PlantVal = 60;
+        public const int MeatVal = 250;
+        public const int MutationChance= 4;
+        public const float MutationAmount = 0.1f;
+        public const int MaxSpeed = 10;
+        public const float MaxSize = 2.0f;
+        public const float MinSize = 2.0f;
+        public const float HerbivoreThreshold = 0.3f;
+        public const float CarnivoreThreshold = 0.7f;
+        public const int FoodChance = 7;//change these values to see different effects
     }
 }
